9e13277ab62f1c9e2ac991f699c1265249b73f6f
Daniel Wozniak <dan@woz.io>
2017-09-08 23:37:36 +0000

    Remove cruft

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup
 
-test_requires = [
-    'pytest',
-]
-
 setup(
     name = 'trusona-pizza-client',
-    #py_modules = ['app'],
     packages = ['pizza_client'],
     entry_points = {
         'console_scripts': ['pizza-client=pizza_client.app:main'],
@@ -16,7 +11,6 @@ setup(
         'An implimentation of the Trusona pizza client'
     ),
     author = 'Daniel Wozniak',
-#    description_file = 'README.md',
     author_email = 'dan@woz.io',
     url = 'https://github.com/dwoz/trusona-pizza-client',
     classifiers=[
